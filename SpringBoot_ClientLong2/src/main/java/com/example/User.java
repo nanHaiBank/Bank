1bf500e6333d2e63f7675dc99a3b26c3e3a80887
root <tangjian@root.co>
2018-01-05 11:10:14 +0800

    112

--- SpringBoot_ClientLong2/src/main/java/com/example/User.java
+++ SpringBoot_ClientLong2/src/main/java/com/example/User.java
@@ -1,13 +1,24 @@
 package com.example;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.whalin.MemCached.MemCachedClient;
+
 
 @Controller
 public class User {
 
+	@Resource
+	MemCachedClient  memCachedClient;
+	
 	@RequestMapping("/add")
 	public String add(){
+		memCachedClient.set("123", 321);
+		Integer str = (Integer) memCachedClient.get("123");
+		System.out.println(str);
 		return "首页";
 	}
 
