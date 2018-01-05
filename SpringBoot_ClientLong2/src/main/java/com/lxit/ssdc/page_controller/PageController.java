f68a88b067238d63e3942438364d0981d454237a
root <tangjian@root.co>
2018-01-05 19:08:00 +0800

    11

--- SpringBoot_ClientLong2/src/main/java/com/lxit/ssdc/page_controller/PageController.java
+++ SpringBoot_ClientLong2/src/main/java/com/lxit/ssdc/page_controller/PageController.java
@@ -1,11 +1,9 @@
 package com.lxit.ssdc.page_controller;
 
-import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.whalin.MemCached.MemCachedClient;
 
 @Controller
 public class PageController {
