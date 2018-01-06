302b42fb436c29cd1507126df87cf7fd1f916e53
root <tangjian@root.co>
2018-01-06 20:30:14 +0800

    11111

---
+++ SpringBoot_ClientLong2/src/main/java/com/lxit/ssdc/server_controller/PandectService.java
@@ -0,0 +1,19 @@
+package com.lxit.ssdc.server_controller;
+
+import java.util.Map;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Component
+//指定eureka客戶端的服务器名称
+@FeignClient("tangjian")
+public interface PandectService {
+	
+	@RequestMapping("/getProjects")
+	@ResponseBody
+	public Map<String,Object> getProjects(String contiding);
+}
